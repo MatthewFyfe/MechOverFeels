14378cf6b733a47b89c1c311442bf014b8ecc4ef
Matthew Fyfe <matthew_fyfe@hotmail.com>
2026-09-19 15:13:34 -0600

    Balance meter

--- mech-over-feels/MechController.gd
+++ mech-over-feels/MechController.gd
@@ -9,18 +9,26 @@ extends CharacterBody3D
 
 @export var body_path: NodePath
 @export var torso_path: NodePath
-@export var camera: Camera3D
 
 @export var playerNumber: int
+@export var balanceOrbUI: Sprite2D
+var balanceOrbHome: Vector2
 
 var _body: Node3D
 var _torso: Node3D
 var _torso_yaw_offset: float = 0.0  # relative to body
 
+var balance_level: Vector2 = Vector2(0.0,0.0)
+var balance_limit_radius: float = 75.0
+var balance_limit_max: float = 128.0
+var drift_vector: Vector2 = Vector2(0.0,0.0)
+var drift_limit_max: float = 0.75
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_body = get_node(body_path)
 	_torso = get_node(torso_path)
+	balanceOrbHome = balanceOrbUI.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -28,6 +36,7 @@ func _process(delta: float) -> void:
 	_handle_movement(delta)
 	_handle_torso_rotation(delta)
 	_handle_weapons(delta)
+	_handle_balance(delta)
 	
 func _apply_gravity(delta: float) -> void:
 	if not is_on_floor():
@@ -46,10 +55,21 @@ func _handle_movement(delta: float) -> void:
 	
 	# Rotate body (tank-style)
 	_body.rotate_y(deg_to_rad(-turn * turn_speed * delta))
+	# Adjust balance
+	if(turn > 0):
+		drift_vector += Vector2(-0.05*delta,0)
+	elif(turn < 0):
+		drift_vector += Vector2(0.05*delta,0)	
 
 	# Move in body's forward direction
 	var forward_dir := -_body.global_transform.basis.x
 	velocity = forward_dir * (forward * move_speed)
+	# Adjust balance
+	if(forward > 0):
+		drift_vector += Vector2(0,0.05*delta)
+	elif(forward < 0):
+		drift_vector += Vector2(0,-0.05*delta)
+	
 	_apply_gravity(delta)
 	move_and_slide()
 
@@ -61,6 +81,8 @@ func _handle_torso_rotation(delta: float) -> void:
 	if(playerNumber == 2):
 		aim_input = Input.get_action_strength("aim_right_p2") - Input.get_action_strength("aim_left_p2")
 	
+	# Simply rotating the torso does not cause imbalance
+	
 	_torso_yaw_offset += -aim_input * torso_turn_speed * delta
 
 	# Clamp torso yaw relative to body
@@ -69,7 +91,7 @@ func _handle_torso_rotation(delta: float) -> void:
 	# Apply rotation (relative to body)
 	_torso.rotation.y = deg_to_rad(_torso_yaw_offset)
 	
-func _handle_weapons(delta: float) -> void:	
+func _handle_weapons(delta: float) -> void:
 	var firing_left
 	var firing_right
 	
@@ -80,6 +102,31 @@ func _handle_weapons(delta: float) -> void:
 		firing_left = Input.get_action_strength("fire_left_p2");
 		firing_right = Input.get_action_strength("fire_right_p2");
 	
+	# Firing weapon affects balance based on torso rotation!
+	if(firing_left > 0):
+		drift_vector += Vector2(0.1*delta, -0.025*delta)
+	if(firing_right > 0):
+		drift_vector += Vector2(-0.1*delta, -0.025*delta)
+	
 	_torso_yaw_offset += (firing_left*torso_left_recoil_speed - firing_right*torso_right_recoil_speed) * delta;
 	_torso_yaw_offset = clamp(_torso_yaw_offset, -max_torso_yaw, max_torso_yaw)
 	_torso.rotation.y = deg_to_rad(_torso_yaw_offset)
+
+func _handle_balance(delta: float) ->  void:
+	#apply our drift vector for this frame
+	balance_level += drift_vector.limit_length(drift_limit_max)
+	
+	#update position of balance orb UI
+	balanceOrbUI.position = balanceOrbHome + balance_level.limit_length(balance_limit_max)
+	
+	#check if we are falling over
+	if(abs(balance_level.distance_to(balanceOrbHome))) > balance_limit_radius:
+		balanceOrbUI.modulate = Color.YELLOW
+		#wobble
+	else:
+		balanceOrbUI.modulate = Color.WHITE
+		#normal
+		
+	if(abs(balance_level.distance_to(balanceOrbHome))) > balance_limit_max:
+		balanceOrbUI.modulate = Color.RED
+		#fall over
