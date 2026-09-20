0de35e0558c1c1e9bba6f243d59b8b1b910cc2e5
Matthew Fyfe <matthew_fyfe@hotmail.com>
2026-09-19 20:51:39 -0600

    Main menu and game loop returns to menu after 3 points

--- mech-over-feels/Scenes/TTS_Test.gd
+++ mech-over-feels/Scenes/TTS_Test.gd
@@ -1,6 +1,9 @@
 extends Node3D
 class_name TTS_Test
 
+@export var scoreLimit = 3
+@export var victoryLabel: Label
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Pick a voice. Here, we arbitrarily pick the first English voice.
@@ -21,4 +24,16 @@ func reset(score1, score2) -> void:
 	print("reset")
 	await get_tree().create_timer(2.0).timeout # Waits for X seconds
 	get_tree().paused = false
-	pass
+	
+	if(score1 >= scoreLimit):
+		victoryLabel.visible = true
+		victoryLabel.text = "BLUE WINS!"
+		victoryLabel.add_theme_color_override("font_color", Color.SKY_BLUE)
+		await get_tree().create_timer(2.0).timeout # Waits for X seconds
+		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+	if(score2 >= scoreLimit):
+		victoryLabel.visible = true
+		victoryLabel.text = "RED WINS!"
+		victoryLabel.add_theme_color_override("font_color", Color.DEEP_PINK)
+		await get_tree().create_timer(2.0).timeout # Waits for X seconds
+		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
