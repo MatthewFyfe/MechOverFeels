8278c861c149441547a605b21a383af612f908e3
Matthew Fyfe <matthew_fyfe@hotmail.com>
2026-09-19 21:49:24 -0600

    fiddling with tts

--- mech-over-feels/Scenes/TTS_Test.gd
+++ mech-over-feels/Scenes/TTS_Test.gd
@@ -4,16 +4,22 @@ class_name TTS_Test
 @export var scoreLimit = 3
 @export var victoryLabel: Label
 
+var tts_volume = 50
+var tts_pitch = 0.8
+var tts_speed = 1.2
+var voice_id
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Pick a voice. Here, we arbitrarily pick the first English voice.
 	var voices = DisplayServer.tts_get_voices_for_language("en")
-	var voice_id = voices[1]
+	voice_id = voices[1]	
 	
-	DisplayServer.tts_speak("Reactor, online.", voice_id)
-	DisplayServer.tts_speak("Sensors, online.", voice_id)
-	DisplayServer.tts_speak("Weapons, online.", voice_id)
-	DisplayServer.tts_speak("All systems nominal.", voice_id)
+	#DisplayServer.tts_speak("Reactor, online.", voice_id, tts_volume, tts_pitch, tts_speed)
+	#DisplayServer.tts_speak("Sensors, online.", voice_id, tts_volume, tts_pitch, tts_speed)
+	#DisplayServer.tts_speak("Weapons, online.", voice_id, tts_volume, tts_pitch, tts_speed)
+	DisplayServer.tts_speak("All systems nominal.", voice_id, tts_volume, tts_pitch, tts_speed)
+	DisplayServer.tts_speak("Mekka-Tumble Rumble, GO!", voice_id, tts_volume, tts_pitch, 1.0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -28,12 +34,14 @@ func reset(score1, score2) -> void:
 	if(score1 >= scoreLimit):
 		victoryLabel.visible = true
 		victoryLabel.text = "BLUE WINS!"
+		DisplayServer.tts_speak("BLUE WINS!", voice_id, tts_volume, tts_pitch, tts_speed)
 		victoryLabel.add_theme_color_override("font_color", Color.SKY_BLUE)
 		await get_tree().create_timer(2.0).timeout # Waits for X seconds
 		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 	if(score2 >= scoreLimit):
 		victoryLabel.visible = true
 		victoryLabel.text = "RED WINS!"
+		DisplayServer.tts_speak("RED WINS!", voice_id, tts_volume, tts_pitch, tts_speed)
 		victoryLabel.add_theme_color_override("font_color", Color.DEEP_PINK)
 		await get_tree().create_timer(2.0).timeout # Waits for X seconds
 		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
